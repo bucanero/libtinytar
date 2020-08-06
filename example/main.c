fb19a7349d47bfca3c065fecfa34ec0155d46ac6
Damian <dparrino@gmail.com>
2020-08-06 17:52:48 -0300

    Update example

--- example/main.c
+++ example/main.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include <stdio.h>
 
+void list_callback(const char* fileName, unsigned long fileSize, char fileType)
+{
+    printf("Type: (%d) File: %s Size: %ld\n", fileType, fileName, fileSize);
+}
 
 int main(void)
 {
@@ -11,8 +15,8 @@ int main(void)
     // Extract .tar.gz archive contents to /dev_hdd0/tmp/
     untar_gz("/dev_hdd0/tmp/archive.tgz", "/dev_hdd0/tmp");
 
-    // Extract .tar.bz2 archive contents to /dev_hdd0/tmp/
-    untar_bz2("/dev_hdd0/tmp/archive.tar.bz2", "/dev_hdd0/tmp");
+    // Extract .tar.bz2 archive contents to /dev_hdd0/tmp/ with a list callback
+    untarEx_bz2("/dev_hdd0/tmp/archive.tar.bz2", "/dev_hdd0/tmp", &list_callback);
 
     return 0;
 }
